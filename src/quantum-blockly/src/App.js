c65a2f626cd44d93ae6817e50b3961ac1dedad57
RobertDuncan1 <duncanrm2@vcu.edu>
2025-03-25 17:52:21 -0400

    push app and connect4 changes

--- src/quantum-blockly/src/App.js
+++ src/quantum-blockly/src/App.js
@@ -23,12 +23,15 @@ function MainComponent() {
 
   const [game, setGame] = useState(0);
   
-  const changeGame = async () => {
-      if(game === 0) {
+  const changeGame = async (newGame) => {
+      if(newGame === 'tic') {
+        setGame(0);
+      }
+      else if (newGame === 'connect') {
         setGame(1);
       }
-      else {
-        setGame(0);
+      else if (newGame === 'mancala') {
+        setGame(2);
       }
   };
   
@@ -37,7 +40,45 @@ function MainComponent() {
     return (
       <>
       <div className="controls">
-          <button onClick={changeGame}>Change Game</button>
+        <label>
+          Choose Game :
+          <select value={game} onChange={(e) => changeGame(e.target.value)}>
+            <option value="tic">Tic-Tac-Toe</option>
+            <option value="connect">Connect 4</option>
+            <option value="mancala">Mancala</option>
+          </select>
+        </label>
+        </div>
+      <div className="main">
+        <div className="vertical-div">
+          <BlocklyComponent mainCodeHandlingFunction={codeHandler} log={logHandler}/>
+          <DisplayComponent heading="Generated Code" text={code} bColor='black'/>
+        </div>
+        <div className="vertical-div" >
+          <div style={{height:'50%'}}>
+            <TicTacToe quboCode={code} log={logHandler}/>
+          </div>
+          <div style={{width: '90%', height:'50%'}}>
+            <DisplayComponent heading="Log" text={log} bColor='black' />
+          </div>
+          
+        </div>
+      </div>
+      </>
+    )
+  }
+  else if (game === 1) {
+    return (
+      <>
+      <div className="controls">
+        <label>
+          Choose Game :
+          <select value={game} onChange={(e) => changeGame(e.target.value)}>
+            <option value="connect">Connect 4</option>
+            <option value="tic">Tic-Tac-Toe</option>
+            <option value="mancala">Mancala</option>
+          </select>
+        </label>
         </div>
       <div className="main">
         <div className="vertical-div">
@@ -57,11 +98,18 @@ function MainComponent() {
       </>
     )
   }
-  else {
+  else if (game === 2) {
     return (
       <>
       <div className="controls">
-          <button onClick={changeGame}>Change Game</button>
+        <label>
+          Choose Game :
+          <select value={game} onChange={(e) => changeGame(e.target.value)}>
+            <option value="mancala">Mancala</option>
+            <option value="tic">Tic-Tac-Toe</option>
+            <option value="connect">Connect 4</option>
+          </select>
+        </label>
         </div>
       <div className="main">
         <div className="vertical-div">
@@ -70,7 +118,7 @@ function MainComponent() {
         </div>
         <div className="vertical-div" >
           <div style={{height:'50%'}}>
-            <TicTacToe quboCode={code} log={logHandler}/>
+            <h2>Placeholder</h2>
           </div>
           <div style={{width: '90%', height:'50%'}}>
             <DisplayComponent heading="Log" text={log} bColor='black' />
